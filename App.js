70f38dd02ee1b31758c0260f5195503ffc713ce2
RUNE-dotcom <laarnz.98@gmail.com>
2025-11-01 14:42:54 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,5 @@
+pg13-g3-R16-nps.js@WatchMovies-Shows-Cartoons
+Expo.goExtension<1:0><90:00>Minutes.RunMachine
+ExpoAndroid.go<PlayStoreLicense><AAppstoreLicense>
+  RunInAndroid[]0092ApacheLicenseF92>CommiteForTvShowsInAAppsMustBeIn
+APIMode<ApkExpoDownload
